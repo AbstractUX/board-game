b3b97b559e09eb2851feb0d0bda7ac25f2d5ac6b
CC <regalforte@gmail.com>
2020-05-10 12:46:55 -0400

    Add dice roll logic and began creating landing on space effects

--- src/components/Boardgame.js
+++ src/components/Boardgame.js
@@ -7,13 +7,14 @@ class Boardgame extends React.Component {
     boardData: [],
     player1data: {
       space: 0,
-      money: 0
+      money: 100
     },
     player2data: {
       space: 0,
-      money: 0
+      money: 100
     },
-    whoseTurn: 'player1'
+    whoseTurn: 'Player 1',
+    messageDisplay: null
   }
 
   generateRandomBoardData = () => {
@@ -39,9 +40,78 @@ class Boardgame extends React.Component {
     this.generateRandomBoardData();
   }
 
-  rollDice = () => {
+  rollDice = (whoRolled) => {
     let roll = Math.floor(Math.random() * 6) + 1;
-    console.log('you rolled a ' + roll);
+    let whoseTurn = this.state.whoseTurn;
+    if (whoRolled === 'Player 1') {
+      this.setState((prevState) => ({
+        whoseTurn: 'Player 2',
+        messageDisplay: `Player 1 rolled a ${roll}`,
+        player1data: {...prevState.player1data, space: (roll + prevState.player1data.space) % 24}
+      }), () => {
+        this.checkSpaceLanded(whoseTurn);
+      });
+    } else {
+      this.setState((prevState) => ({
+        whoseTurn: 'Player 1',
+        messageDisplay: `Player 2 rolled a ${roll}`,
+        player2data: {...prevState.player2data, space: (roll + prevState.player2data.space) % 24}
+      }), () => {
+        this.checkSpaceLanded(whoseTurn);
+      });
+    }
+  }
+
+  checkSpaceLanded = (whichPlayer) => {
+    if (whichPlayer === 'Player 1') {
+      switch (this.state.boardData[this.state.player1data.space]) {
+        case 'earn':
+          this.setState((prevState) => ({
+            player1data: {...prevState.player1data, money: prevState.player1data.money + 100},
+            messageDisplay: prevState.messageDisplay + ' and lands on a blue coin space, earning $100.'
+          }));
+          break;
+        case 'lose':
+          this.setState((prevState) => ({
+            player1data: {...prevState.player1data, money: prevState.player1data.money - 100},
+            messageDisplay: prevState.messageDisplay + ' and lands on a red coin space, losing $100.'
+          }));
+          break;
+        case 'powerup':
+          console.log('powerup');
+          break;
+        case 'skull':
+          this.setState((prevState) => ({
+            player1data: {...prevState.player1data, money: Math.floor(prevState.player1data.money / 2)},
+            messageDisplay: prevState.messageDisplay + ' and lands on a skull, losing half their money.'
+          }));
+          break;
+      }
+    } else {
+      switch (this.state.boardData[this.state.player2data.space]) {
+        case 'earn':
+          this.setState((prevState) => ({
+            player2data: {...prevState.player2data, money: prevState.player2data.money + 100},
+            messageDisplay: prevState.messageDisplay + ' and lands on a blue coin space, earning $100.'
+          }));
+          break;
+        case 'lose':
+        this.setState((prevState) => ({
+          player2data: {...prevState.player2data, money: prevState.player2data.money + 100},
+          messageDisplay: prevState.messageDisplay + ' and lands on a red coin space, losing $100.'
+        }));
+          break;
+        case 'powerup':
+          console.log('powerup');
+          break;
+        case 'skull':
+          this.setState((prevState) => ({
+            player2data: {...prevState.player2data, money: Math.floor(prevState.player2data.money / 2)},
+            messageDisplay: prevState.messageDisplay + ' and lands on a skull, losing half their money.'
+          }));
+          break;
+      }
+    }
   }
 
   render() {
@@ -52,7 +122,9 @@ class Boardgame extends React.Component {
           <h2 className="score" style={{backgroundColor: 'lightgreen'}}>Player 2: ${this.state.player2data.money}</h2>
           <Board boardData={this.state.boardData} player1data={this.state.player1data} player2data={this.state.player2data} />
         </div>
-        <button onClick={this.rollDice}>Roll dice</button>
+        <h1>{this.state.whoseTurn}'s turn to roll.</h1>
+        <h2>{this.state.messageDisplay}</h2>
+        <button onClick={() => this.rollDice(this.state.whoseTurn)}>Roll dice</button>
       </div>
     )
   }
